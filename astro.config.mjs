6b3110663ce323dd403d06296f10f85c5a84aa5c
Mark Njoroge <marknjo7@gmail.com>
2020-12-09 08:33:19 +0300

    feat(api): configure partytown in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog.vercel.app",
-  integrations: [sitemap()],
+  integrations: [sitemap(), partytown()],
 })
