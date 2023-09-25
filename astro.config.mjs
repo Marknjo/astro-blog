5ca40c7d891fe445c7ee035c139cfc1b9d3bb4aa
Mark Njoroge <marknjo7@gmail.com>
2023-09-25 08:49:23 +0300

    fix astro.config - failed to import partytown package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config"
 
 import sitemap from "@astrojs/sitemap"
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
